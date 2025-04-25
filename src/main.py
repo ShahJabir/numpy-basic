d36a3680d3bdd32b9d8ffc11475966111e2d54d3
ShahJabir <shahjabirtaqi@gmail.com>
2025-04-25 14:43:23 +0600

    basic array and list create and thier multiplication

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import numpy as np
+
+#  Creating array from list
+arr_1d = np.array([1,2,3,4,5,6])
+arr_2d = np.array([[1,2,3],[4,5,6]])
+
+# List vs numpy array
+py_list = [1,2,3]
+
 def main():
-    print("Hello from numpy-basic!")
+    print("1D Array: ", arr_1d)
+    print("2D Array: ", arr_2d)
+    print("Python list multiplication: ", py_list*2)
+    print("Python Array Multiplication: ", arr_1d*2)
 
 
 if __name__ == "__main__":
